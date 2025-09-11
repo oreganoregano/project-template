61a66ec0f927c9a0c560288ab57d4bf0c2e360f4
Regan Smith <33164588+oreganoregano@users.noreply.github.com>
2025-09-11 11:27:21 -0400

    Adds username input for assignment 2 (#3)
    Username input accepts text. Username is displayed under the input box when username is not empty. Reset button is enabled when username is not empty and clears the string when clicked.

---
+++ src/app/assignment2/username/username.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username',
+  templateUrl: './username.component.html',
+  styleUrl: './username.component.css',
+})
+export class UsernameComponent {
+  username = '';
+
+  onResetUsername() {
+    this.username = '';
+  }
+}
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,27 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: "app-server",
-    templateUrl: "./server.component.html"
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [
+    `
+      color: white;
+    `,
+  ],
 })
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  server: string = '';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
